52c905a442d2b3b913665297d1d2d98088bd7e77
Ellen Graham <grahamammal@gmail.com>
2021-01-28 16:10:15 -0600

    removed superflous function

--- Code/rsrHonors/R/rrp_glm.R
+++ Code/rsrHonors/R/rrp_glm.R
@@ -388,15 +388,6 @@ covfndef <- function(nu){
   return(covfn)
 }
 
-# generate multivariate normal
-rmvn <- function(n, mu = 0, V = matrix(1)){
-  p <- length(mu)
-  if (any(is.na(match(dim(V), p))))
-    stop("Dimension problem!")
-  D <- chol(V)
-  t(matrix(rnorm(n*p), ncol = p) %*% D + rep(mu, rep(n, p)))
-}
-
 
 beta_log_full_conditional <- function(beta, current_w, X,
                                       O,
